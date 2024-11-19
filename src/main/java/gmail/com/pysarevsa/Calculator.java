678c2242cb6e98d8a5c9c992bed167e75501dfb5
Serhii Pysarev <pysarevsa@gmail.com>
2024-11-19 02:03:31 +0100

    Тестовый коммит

---
+++ src/main/java/gmail/com/pysarevsa/Calculator.java
@@ -0,0 +1,33 @@
+package gmail.com.pysarevsa;
+
+import static java.lang.Math.sqrt;
+
+public class Calculator { // калькулятор
+    public static float runCalculate(float first, String simvol, float second) {
+
+        switch (simvol) { //тело вычислителя
+            case "+":
+                return first + second;
+            case "-":
+                return first - second;
+            case "*":
+                return first * second;
+            case "/":
+                if (second == 0) {
+                    System.out.println("Делитель не может быть равен \"0\". \nНа ноль, делить нельзя!!!");
+                } else {
+                    return first / second;
+
+                }
+            default:
+                return 0;
+        }
+
+    }
+
+    public static float runSqrt(float first) {  // вычисление корня
+        return (float) sqrt(first);
+
+    }
+
+}
---
+++ src/main/java/gmail/com/pysarevsa/Main.java
@@ -0,0 +1,87 @@
+package gmail.com.pysarevsa;
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import static java.lang.Math.sqrt;
+
+public class Main {
+    public static void main(String[] args) {
+        String HELLO = "Добрый день, я калькулятор. \nСписок поддерживаемых команд можно посмотреть по запросу \"911\"";
+        String warning = "Делитель не может быть равен \"0\". \nНа ноль, делить нельзя!!!";
+        String warning2 = "Допустимые действия - \"+\" \"-\"  \"/\"  \"*\"  \"sqrt\"";
+        String help = "Тут будут инструкции";
+        String otvet = "Результат = ";
+        String number1 = "Введите первое значение:";
+        String number2 = "Введите второе значение:";
+        String znak = "Введите действие: [-] [+]  [/] [*] [sqrt]";
+        String checkPattern = "^[+\\-\\*/sqrt]+$";
+        String textkoren = "Корень числа ";
+
+
+        Scanner in = new Scanner(System.in); //Ввод данных
+        System.out.println(HELLO);
+
+        float first;
+        String simvol;
+        float second;
+
+        System.out.println(number1); //просим ввести первое число
+        while (true) {
+            try {
+                first = in.nextFloat();
+
+                break;
+            } catch (InputMismatchException e) {
+
+                System.out.println("Ошибка! Вы ввели некорректное значение. Попробуйте снова.");
+                in.nextLine();
+            }
+        }
+
+        if (first == 911) {  //пытаемся вывести разде HELP
+            System.out.println(help);
+            System.exit(0);
+        }
+
+        in.nextLine(); // очистка буфера
+
+        System.out.println(znak);// просим знак
+
+        while (true) {
+            simvol = in.nextLine(); // знак действия
+
+            if (Pattern.matches(checkPattern, simvol)) {
+                break;
+            } else {
+                System.out.println("Некорректный ввод знака");
+            }
+        }
+
+        if (simvol.equals("sqrt")) {  //вычисления корня
+            float sqrtOUT = Calculator.runSqrt(first);
+            System.out.println(textkoren + first + " = " + sqrtOUT);
+            System.exit(0);
+        }
+        System.out.println(number2); //просим ввести второе число
+        while (true) {
+            try {
+                second = in.nextFloat();
+
+                break;
+            } catch (InputMismatchException e) {
+
+                System.out.println("Ошибка! Вы ввели некорректное значение. Попробуйте снова.");
+                in.nextLine();
+            }
+        }
+
+        float resalt = Calculator.runCalculate(first, simvol, second);
+
+        System.out.println(otvet + resalt);
+
+
+    }
+}
